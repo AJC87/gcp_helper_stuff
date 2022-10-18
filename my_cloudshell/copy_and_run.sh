31e5ef3c03c42f095d5feb097253ca3dfde40e1e
Ashley Craig <ashleycraig19872@hotmail.co.uk>
2022-10-18 23:43:55 +0000

    Build scripts out - add shell checking, start of writing stuff to login file

--- my_cloudshell/copy_and_run.sh
+++ my_cloudshell/copy_and_run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # Copy .customize_environment to required location and run it
 
-cp .customize_environment $HOME/.customize_environment
+cp .customize_environment "${HOME}/.customize_environment"
 
-# TODO: run it
+sh "${HOME}/.customize_environment"
 
-# For .bashrc
+if [[ "${SHELL}" =~ 'bash' ]]; then
+    readonly LOGIN_FILE='.bashrc'
+elif [[ "${SHELL}" =~ 'zsh' ]]; then
+    readonly LOGIN_FILE='.zshrc'
+else
+    echo 'Shell not recognised, exiting...'
+fi
+
+echo "${LOGIN_FILE}"
+
+# cat << EOF >> "${HOME}/${LOGIN_FILE}"
 # export EDITOR='nano'
 # alias k='kubectl'
+# EOF
